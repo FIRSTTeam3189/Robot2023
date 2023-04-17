b53c8b541ae5f6dac1dac45b76ae6263193bb1ed
Ethan Pham <ethansaddy@gmail.com>
2023-04-17 00:06:06 -0700

    Commented out ultrashoot for now

--- src/main/cpp/commands/UltraShoot.cpp
+++ src/main/cpp/commands/UltraShoot.cpp
@@ -11,28 +11,29 @@ UltraShoot::UltraShoot(Elevator *elevator, Intake *intake, Grabber *grabber)
   AddRequirements(intake);
   AddRequirements(grabber);
 
-  AddCommands(
-    // Extend pistons
-    frc2::InstantCommand([this]{ m_intake->SetPistonExtension(true);},{m_intake}),
-    frc2::ParallelDeadlineGroup(
-      frc2::WaitCommand(0.5_s),
-      RunIntake(m_intake, -INTAKE_ROLLER_POWER, -INTAKE_CONVEYOR_POWER)
-    ),
-    // Sends elevator to max height
-    // During the trip up, checks if the grabber is above the release point
-    // If it is, the grabber will shoot the piece while the elevator is rising to use momentum
-    frc2::ParallelDeadlineGroup(
-      ElevatorPID(m_elevator, ELEVATOR_ULTRA_SHOOT_TARGET, false),
-      frc2::RunCommand([this]{
-        if (m_elevator->GetPosition() > ELEVATOR_ULTRA_SHOOT_RELEASE_POINT) {
-          m_grabber->SetSpeed(ELEVATOR_ULTRA_SHOOT_POWER);
-        }
-      },{m_grabber})
-    ),
-    // Send the elevator back to the base and retract pistons
-    frc2::ParallelDeadlineGroup(
-      frc2::WaitCommand(2.0_s),
-      ElevatorPID(m_elevator, 0, false)),
-    frc2::InstantCommand([this]{ m_intake->SetPistonExtension(false);},{m_intake})
-  );
+  // Fix crashing -- either command grouping or subsystem requirements likely
+  // AddCommands(
+  //   // Extend pistons
+  //   frc2::InstantCommand([this]{ m_intake->SetPistonExtension(true);},{m_intake}),
+  //   frc2::ParallelDeadlineGroup(
+  //     frc2::WaitCommand(0.5_s),
+  //     RunIntake(m_intake, -INTAKE_ROLLER_POWER, -INTAKE_CONVEYOR_POWER)
+  //   ),
+  //   // Sends elevator to max height
+  //   // During the trip up, checks if the grabber is above the release point
+  //   // If it is, the grabber will shoot the piece while the elevator is rising to use momentum
+  //   frc2::ParallelDeadlineGroup(
+  //     ElevatorPID(m_elevator, ELEVATOR_ULTRA_SHOOT_TARGET, false),
+  //     frc2::RunCommand([this]{
+  //       if (m_elevator->GetPosition() > ELEVATOR_ULTRA_SHOOT_RELEASE_POINT) {
+  //         m_grabber->SetSpeed(ELEVATOR_ULTRA_SHOOT_POWER);
+  //       }
+  //     },{m_grabber})
+  //   ),
+  //   // Send the elevator back to the base and retract pistons
+  //   frc2::ParallelDeadlineGroup(
+  //     frc2::WaitCommand(2.0_s),
+  //     ElevatorPID(m_elevator, 0, false)),
+  //   frc2::InstantCommand([this]{ m_intake->SetPistonExtension(false);},{m_intake})
+  // );
 }
